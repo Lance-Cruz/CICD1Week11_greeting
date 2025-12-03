25b55f15e299f54a7862f0770407b635afa59c78
LANCECRUZ-STUDENT <g00428876@atu.ie>
2025-12-03 13:26:37 +0000

    Initial Commit

---
+++ src/main/java/ie/atu/greetingservice/Week11GreetingServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.greetingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week11GreetingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week11GreetingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/greetingservice/Week11GreetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.greetingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week11GreetingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
